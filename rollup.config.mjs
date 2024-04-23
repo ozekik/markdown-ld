716801617e1856c11c2423727d9a11c80f6dd42b
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2024-04-24 00:35:36 +0900

    update

---
+++ rollup.config.mjs
@@ -0,0 +1,29 @@
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+
+import pkg from "./package.json" assert { type: "json" };
+
+export default {
+  input: "lib/main.js",
+  output: [
+    { file: pkg.main, format: "cjs", exports: "default" },
+    { file: pkg.module, format: "es" },
+  ],
+  plugins: [
+    replace({
+      delimiters: ["", ""],
+      // NOTE: Hack for @frogcat/ttl2jsonld
+      // TODO: Send PR
+      [`var URL = typeof require === 'undefined' ? URL : require("url").URL;`]:
+        "",
+    }),
+    resolve({
+      // browser: true,
+      // preferBuiltins: false,
+    }),
+    commonjs(),
+    json(),
+  ],
+};
